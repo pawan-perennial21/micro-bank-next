bd611f4faac6f3d8771311542fdbccffee3428a5
pawanperennial21 <pawan.patidar@perennialsys.com>
2023-03-15 17:01:52 +0530

    add the eslint and prettier setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-    return <div>Micro bank</div>;
+  return <div>Micro bank</div>;
 };
 
 export default Home;
